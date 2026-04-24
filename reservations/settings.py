f09f65d2f8687ba478af3144206c487054f845f1
Malcolm MikaZuki <65243328+MikaCodez@users.noreply.github.com>
2026-04-24 11:57:10 +0000

    push host changes

--- reservations/settings.py
+++ reservations/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ghit-reservation.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'ghitrestaurant.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 render_hostname = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if render_hostname:
